fc1c3280764261dcd0651e8523db2096f21e7214
Michel <michaelhijazeen203@gmail.com>
2023-01-28 17:05:27 -0500

    Wrote request

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 from bs4 import BeautifulSoup
+import requests
 import utilities
 
 app = Flask(__name__)
@@ -9,9 +10,13 @@ def home(): #home page
     if request.method == 'POST':
         ingred1 = request.form.get('ingred1')
         provIng1 = "https://www.provigo.ca/search?search-bar="+ingred1
+        r1 = requests.get(provIng1)
 
         ingred2 = request.form.get('ingred2')
         provIng2 = "https://www.provigo.ca/search?search-bar="+ingred2
+        r2 = requests.get(provIng2)
+
+
 
 
     return render_template('index.html')
