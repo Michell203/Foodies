307e21c41cc30b4eae9c262922b38c0ef0b95800
Michel <michaelhijazeen203@gmail.com>
2023-01-28 20:19:28 -0500

    Removed unecessary code

--- app.py
+++ app.py
@@ -26,40 +26,8 @@ def home(): #home page
         print(metroPirce)
 
         
-        #return ingPirce
-    return render_template('index.html')
-
-
-    if request.method == 'POST':
-        # ingred1 = request.form.get('ingred1')
-        provIng1 = "https://www.provigo.ca/search?search-bar=chicken"
-        # provIng1 = "https://www.provigo.ca/search?search-bar="+str(ingred1)
-        # print(provIng1)
-        r1 = requests.get(provIng1)
-        # r1T = r1.text
-        # print(r1T)
-
-        # soup1 = BeautifulSoup(r1.content, 'html.parser')
-        soup1 = BeautifulSoup(r1.content, 'lxml')
-        print(list(soup1)[-1000:])
-
-        # price = soup1.find_all("span",class_="price__value")
-        # price = soup1.find("span",class_="price__value")
-        price = soup1.find("div",class_="ad-unit-link-wrapper")
-        print(price)
-
-        # ingred2 = request.form.get('ingred2')
-        # provIng2 = "https://www.provigo.ca/search?search-bar="+ingred2
-        # r2 = requests.get(provIng2)
-        print("in if")
-        return provIng1
         
-    print("bruh")
     return render_template('index.html')
-
-# @app.route("/ingreds", method = ['GET', 'POST'])
-# def ingreds():
-#     return render_template('ingredients.html')
 
 if __name__ == "__main__":
     app.run()
